ef2b9b4a6124532bb201ef7e074bf3339119d01b
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2021-01-25 20:09:16 -0300

    add XML module in starterjs

--- starterjs/starterJS-0.1.x.js
+++ starterjs/starterJS-0.1.x.js
@@ -3788,6 +3788,78 @@ ReaderCSVFile.prototype.getValue = function (x, y) {
 }
 
 
+/**
+* Essa classe é responsávl pela leitura de arquivos xml
+* @class
+*/
+function ReaderXMLFile(path) {
+
+    this.xml = null;
+
+    if(path!=undefined){
+        this.xml = se.loader.getAssets(path);
+    }
+
+    if(this.xml.getElementsByTagName("parsererror").length > 0 )
+        throw new Error("O seu arquivo XML possui algum erro!");
+
+
+}
+
+
+ReaderXMLFile.prototype.getNode = function(tag, index){
+    if(index == undefined)
+        index = 0;
+
+    return this.xml.getElementsByTagName(tag)[index].childNodes[0].nodeValue;
+}
+
+ReaderXMLFile.prototype.getCount = function(tag){
+    return this.xml.getElementsByTagName(tag).length;
+}
+
+
+/**
+* Essa classe é responsávl pela leitura de arquivos textos
+* @class
+*/
+function ReaderTextFile(path) {
+    this.text = [];
+    this.currentLine = 0;
+
+    if(path!=undefined){
+        this.text = se.loader.getAssets(path);
+    }
+}
+
+
+/**
+ * Ler a próxima linha do arquivo
+ * @return {string} text - linha do arquivo
+ */
+ReaderTextFile.prototype.nextLine = function () {
+    return this.text[this.currentLine++];
+}
+
+/**
+ * Ler a linha atual do arquivo
+ * @return {string} text - linha do arquivo
+ */
+ReaderTextFile.prototype.priorLine = function () {
+    return this.text[--this.currentLine];
+}
+
+/**
+ * Ler uma linha especifica do arquivo
+ * @return {string} text - linha do arquivo
+ */
+ReaderTextFile.prototype.getLine = function (index) {
+    if(index < this.text.length)
+        return this.text[index];
+}
+
+
+
 
 
 
