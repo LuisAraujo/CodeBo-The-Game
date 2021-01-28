a112e7cce28ced1b1be09c541393b6b52142725a
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2021-01-28 19:12:26 -0300

    fix render level
    render fixed to all levels

--- scripts/block.js
+++ scripts/block.js
@@ -1,8 +1,9 @@
-function Block(sprite, x, y, classename, z) {
+function Block(sprite, x, y, classename, z, level) {
     
 	h = 70;
 	w = 70;
-
+    this.level = level?level:1;
+	
 	GameObject.call(this, sprite, x, y, classename, h, w, 0, z);
    
 }
--- scripts/map.js
+++ scripts/map.js
@@ -6,16 +6,51 @@ function Map(map, margintop, marginleft) {
 	this.create();
 	
 }
+Map.prototype.adjustmentLevels = function(level, z){
+	
+	for(var i = 0; i < this.block.length  ; i++){
+		if(this.block[i].level > z){
+			
+			this.map[i][j].z = 100;
+		}else{
+			
+			this.map[i][j].z = 1;
+		}
+	
+	}
+}
 
-
+Map.prototype.createBlocks = function(i, j){
+	
+	x = j;
+	y = i;
+	
+	posx = 200 + (y*105) - (x*35) + (x-y) * 70;
+	posy = 200 - (y * 17.5) -(x*17.5) + (y+1) * 35;
+	
+	for(var l = 0; l < this.map[i][j]; l++){
+		
+		if( ( l == this.map[i][j]-1) && (l == 0) )
+			blockname = "block_1";
+		else if(l == 0) 
+			blockname = "block_0";
+		else if(l == this.map[i][j]-1) 
+			blockname = "block_2";
+		else 
+			blockname = "block_3";
+		
+		console.log(blockname);
+		new Block( blockname , posx  , posy - 35*l ,"imagem", 1 );
+	}
+	
+}
 Map.prototype.create = function () {
 	
 	var anim1 = new Animation( ["water_0","water_1","water_2"], 0.1);
 	
 	
 	//for(var i = this.map.length-1; i >= 0  ; i--){
-	for(var i = 0; i < this.map.length  ; i++){
-		
+	for(var i = 0; i < this.map.length  ; i++){	
 		for(var j = this.map[i].length-1; j >= 0 ; j--){
 		//for(var j = 0; j < this.map[i].length ; j++){
 			//if(map[i][j] != 0){
@@ -24,35 +59,24 @@ Map.prototype.create = function () {
 				if(this.map[i][j] == 0){
 					
 					//new GameObject( [anim1], this.marginleft + m + j* 70, this.margintop + i * 17.5, "water", 70,35 );
+					
 	
 				//ok
 				}else if(this.map[i][j] == 1){
-					//console.log( "x = " + j, "y = " + i);
-					
-					var x = j; 
-					var y = i;
-
-					new Block( "block_1" ,   200 + (y*105) - (x*35) + (x-y) * 70, 200 - (y * 17.5) -(x*17.5) + (y+1) * 35,"imagem", 1 );
-			   
-					
-				//@todo
+		
+					this.createBlocks(i, j);
+			  	
 				}else if(this.map[i][j] == 2){
-					
-					//new Block( "block_0" , this.marginleft + m + j*70, this.margintop +i*17.5-35, "imagem", 1 );
-					
-					//new Block( "block_1" , this.marginleft + m + j*70, this.margintop +i*17.5-35, "imagem", 1 );
+					this.createBlocks(i, j);
+				
 				//@todo
 				}else if(this.map[i][j] == 3){
 					
 					/*esse blocos estão com um z maior que codebo
 					para dá a ideia de 3d
 					quando codebo mudar de nivel, esse blocos (ou pelo menos o superior, deve ficar com o z menor que o codebo*/
+					this.createBlocks(i, j);
 					
-					//new Block( "block_0" , this.marginleft + m + j*70, this.margintop +i*17.5, "imagem", 100 );
-					
-					//new Block( "block_3" , this.marginleft + m + j*70, this.margintop +i*17.5-35, "imagem", 100 );
-					
-					//new Block( "block_2" , this.marginleft + m + j*70, this.margintop +i*17.5-70, "imagem", 100 );
 				//@todo
 				}else if(this.map[i][j] == 4){
 					
