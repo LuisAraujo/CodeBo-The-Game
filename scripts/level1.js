1adb7a4de54a3b246426e5576d8278898b0a161b
Luan Gomes <luangms@outlook.com>
2021-01-30 10:29:10 -0300

    Reorganize setResources() and change stack pop' button

--- scripts/level1.js
+++ scripts/level1.js
@@ -1,76 +1,78 @@
 function Level1() {
+  this.xmlfile = 'map_level_1';
+  this.scene = new Scene();
+  this.start();
 
-	this.xmlfile = 'map_level_1'
-	this.scene = new Scene();
-	this.start();
+  this.commands = [
+    'stack_new',
+    'stack_block_push',
+    'stack_character_push',
+    'stack_pop',
+  ];
 
-	this.commands = [
-	'stack_new',
-	'stack_block_push',
-	'stack_character_push',
-	'stack_block_pop'
-	];
-  
-	this.codebox = 1;
-	this.codeboy = 2;
+  this.codebox = 1;
+  this.codeboy = 2;
 
-	this.codebo_posx = 315;
-	this.codebo_posy = 200;
+  this.codebo_posx = 315;
+  this.codebo_posy = 200;
 
-	this.map_margintop = 200; 
-	this.map_marginleft = 70;
+  this.map_margintop = 200;
+  this.map_marginleft = 70;
 
-	this.limitcommands = 10;
-	
-	this.codebo = null;
-	this.map;
-	
+  this.limitcommands = 10;
+
+  this.codebo = null;
+  this.map;
 }
 
-
-Level1.prototype.updateLevel = function(limitcommands) {
+Level1.prototype.updateLevel = function (limitcommands) {
   printCommands(limitcommands);
-}
+};
 
-
-Level1.prototype.start = function(){
-	
-  xmlmap = new ReaderXMLFile(this.xmlfile );
+Level1.prototype.start = function () {
+  xmlmap = new ReaderXMLFile(this.xmlfile);
   this.arrmap = createArrayMap(xmlmap);
 
   _this = this;
-  
+
   //configurando as funcoes de inicio e update
   this.scene.setFunctionStart(_this.setLevel.bind(null, _this.arrmap, _this));
-    
-  function teste (_this){
-	  _this.updateLevel(_this.limitcommands)
-  };
-  
-  this.scene.setFunctionUpdate(teste.bind(null, _this ));
-	
-}
 
+  function teste(_this) {
+    _this.updateLevel(_this.limitcommands);
+  }
 
+  this.scene.setFunctionUpdate(teste.bind(null, _this));
+};
 
-Level1.prototype.setLevel = function(arrmap, _this) {
+Level1.prototype.setLevel = function (arrmap, _this) {
   createGUIButton();
-  
-  var commands = _this.commands;
-  
-  
-  //map
-  _this.map = new Map(arrmap, _this.map_margintop , this.map_marginleft );
-  //console.log(lv1.map);
-  
-  //CodeBo
-  _this.codebo = new Codebo(_this.codebo_posx, _this.codebo_posy,  _this.codebox, _this.codeboy, 'play', 99);
-  
-  //_this.codebo.map = arrmap;
-    
-  _this.map.adjustmentLevels(_this.codebo.getLevel(), _this.codebo.actualx, _this.codebo.actualy, _this.map);
 
-  
+  var commands = _this.commands;
+
+  //map
+  _this.map = new Map(arrmap, _this.map_margintop, this.map_marginleft);
+  //console.log(lv1.map);
+
+  //CodeBo
+  _this.codebo = new Codebo(
+    _this.codebo_posx,
+    _this.codebo_posy,
+    _this.codebox,
+    _this.codeboy,
+    'play',
+    99
+  );
+
+  //_this.codebo.map = arrmap;
+
+  _this.map.adjustmentLevels(
+    _this.codebo.getLevel(),
+    _this.codebo.actualx,
+    _this.codebo.actualy,
+    _this.map
+  );
+
   actions = [];
 
   size = 60;
@@ -79,32 +81,23 @@ Level1.prototype.setLevel = function(arrmap, _this) {
   marginbt = 10;
 
   //FIXO DE TODOS OS LEVELS
-  ['forward', 'left', 'right'].forEach( function(item){
-	
-		createCommandsButton( item, _this.limitcommands); 
-  
+  ['forward', 'left', 'right'].forEach(function (item) {
+    createCommandsButton(item, _this.limitcommands);
   });
 
-  commands.forEach(  function(item){
-	
-		createCommandsButton( item, _this.limitcommands); 
-  
-  } );
-   
-}
+  commands.forEach(function (item) {
+    createCommandsButton(item, _this.limitcommands);
+  });
+};
 
-
-Level1.prototype.getScene = function() {
+Level1.prototype.getScene = function () {
   return this.scene;
-}
+};
 
-
-Level1.prototype.getCodebo = function() {
+Level1.prototype.getCodebo = function () {
   return this.codebo;
-}
+};
 
-
-
-Level1.prototype.getMap = function() {
+Level1.prototype.getMap = function () {
   return this.map;
-}
+};
--- scripts/mygame.js
+++ scripts/mygame.js
@@ -1,8 +1,9 @@
 const se = new StarterEngine();
 
-
 se.setResources = function () {
-  //sprites CodeBo pra frente
+  //CHARACTER
+
+  //CodeBo pra frente
   this.loader.addResource('codebo_sp1', 'codebo/forward/sp1.png', 'image');
   this.loader.addResource('codebo_sp2', 'codebo/forward/sp2.png', 'image');
   this.loader.addResource('codebo_sp3', 'codebo/forward/sp3.png', 'image');
@@ -10,17 +11,17 @@ se.setResources = function () {
   this.loader.addResource('codebo_sp5', 'codebo/forward/sp5.png', 'image');
   this.loader.addResource('codebo_sp6', 'codebo/forward/sp6.png', 'image');
 
-  //sprites CodeBo pra trás
+  //CodeBo pra trás
   this.loader.addResource(
     'codebo_back_sp1',
     'codebo/backward/sp1.png',
     'image'
   );
 
-  //sprites CodeBo pra direita
+  //CodeBo pra esquerda
   this.loader.addResource('codebo_left_sp1', 'codebo/left/sp1.png', 'image');
 
-  //sprites CodeBo pra esquerda
+  //CodeBo pra direita
   this.loader.addResource('codebo_right_sp1', 'codebo/right/sp1.png', 'image');
   this.loader.addResource('codebo_right_sp2', 'codebo/right/sp2.png', 'image');
   this.loader.addResource('codebo_right_sp3', 'codebo/right/sp3.png', 'image');
@@ -28,17 +29,37 @@ se.setResources = function () {
   this.loader.addResource('codebo_right_sp5', 'codebo/right/sp5.png', 'image');
   this.loader.addResource('codebo_right_sp6', 'codebo/right/sp6.png', 'image');
 
-  //sprites blocks
+  //BLOCKS
+
+  //earth blocks
   this.loader.addResource('block_0', 'blocks/block_0.png', 'image');
   this.loader.addResource('block_1', 'blocks/block_1.png', 'image');
   this.loader.addResource('block_2', 'blocks/block_2.png', 'image');
   this.loader.addResource('block_3', 'blocks/block_3.png', 'image');
 
-  //sprites water
+  //metal blocks
+  this.loader.addResource('block_4', 'blocks/block_4.png', 'image');
+  //this.loader.addResource('block_blank2', 'blocks/block_blank2.png', 'image');
+
+  //water blocks
   this.loader.addResource('water_0', 'water/water_0.png', 'image');
   this.loader.addResource('water_1', 'water/water_1.png', 'image');
   this.loader.addResource('water_2', 'water/water_2.png', 'image');
 
+  //BUTTONS
+
+  //control buttons
+  this.loader.addResource('button_play', 'buttons/button_play.png', 'image');
+  this.loader.addResource('button_stop', 'buttons/button_stop.png', 'image');
+  this.loader.addResource('button_help', 'buttons/button_help.png', 'image');
+  this.loader.addResource(
+    'button_reload',
+    'buttons/button_reload.png',
+    'image'
+  );
+  this.loader.addResource('button_menu', 'buttons/button_menu.png', 'image');
+
+  //movement buttons
   this.loader.addResource(
     'button_forward',
     'buttons/button_forward.png',
@@ -47,7 +68,7 @@ se.setResources = function () {
   this.loader.addResource('button_left', 'buttons/button_left.png', 'image');
   this.loader.addResource('button_right', 'buttons/button_right.png', 'image');
 
-  //sprites buttons: list
+  //list buttons
   this.loader.addResource(
     'button_list_new',
     'buttons/button_list_new.png',
@@ -58,71 +79,42 @@ se.setResources = function () {
     'buttons/button_list_block_add.png',
     'image'
   );
-
   this.loader.addResource(
     'button_list_block_remove',
     'buttons/button_list_block_remove.png',
     'image'
   );
 
-  //sprites buttons: stack
+  //stack buttons
   this.loader.addResource(
     'button_stack_new',
     'buttons/button_stack_new.png',
     'image'
   );
-
   this.loader.addResource(
     'button_stack_block_push',
     'buttons/button_stack_block_push.png',
     'image'
   );
-
-  this.loader.addResource(
-    'button_stack_block_pop',
-    'buttons/button_stack_block_pop.png',
-    'image'
-  );
-
   this.loader.addResource(
     'button_stack_character_push',
     'buttons/button_stack_character_push.png',
     'image'
   );
-
-  this.loader.addResource(
-    'button_stack_character_pop',
-    'buttons/button_stack_character_pop.png',
-    'image'
-  );
-
   this.loader.addResource(
     'button_stack_pop',
     'buttons/button_stack_pop.png',
     'image'
   );
 
-  //sprites buttons: control
-  this.loader.addResource('button_play', 'buttons/button_play.png', 'image');
-  this.loader.addResource('button_stop', 'buttons/button_stop.png', 'image');
-  this.loader.addResource('button_help', 'buttons/button_help.png', 'image');
-  this.loader.addResource(
-    'button_reload',
-    'buttons/button_reload.png',
-    'image'
-  );
+  //MINI BUTTONS
 
-  this.loader.addResource('button_menu', 'buttons/button_menu.png', 'image');
-
-  //sprites mini buttons: movements
+  //movement mini buttons
   this.loader.addResource(
     'mini_forward',
     'buttons/button_forward_mini.png',
     'image'
   );
-
-  this.loader.addResource('mini_blank', 'buttons/button_mini.png', 'image');
-
   this.loader.addResource('mini_left', 'buttons/button_left_mini.png', 'image');
   this.loader.addResource(
     'mini_right',
@@ -130,7 +122,9 @@ se.setResources = function () {
     'image'
   );
 
-  //sprites mini buttons: list
+  this.loader.addResource('mini_blank', 'buttons/button_mini.png', 'image');
+
+  //list mini buttons
   this.loader.addResource(
     'button_list_new_mini',
     'buttons/button_list_new_mini.png',
@@ -147,23 +141,15 @@ se.setResources = function () {
     'image'
   );
 
-  //sprites mini buttons: stack
+  //stack mini buttons
   this.loader.addResource(
     'mini_stack_new',
     'buttons/button_stack_new_mini.png',
     'image'
   );
-
   this.loader.addResource(
     'mini_stack_block_push',
     'buttons/button_stack_block_push_mini.png',
-    'image'
-  );
-
-  //pop n pode ser relcionado a um item, pop é geral
-  this.loader.addResource(
-    'mini_stack_block_pop',
-    'buttons/button_stack_block_pop_mini.png',
     'image'
   );
   this.loader.addResource(
@@ -171,57 +157,34 @@ se.setResources = function () {
     'buttons/button_stack_character_push_mini.png',
     'image'
   );
-
   this.loader.addResource(
-    'button_stack_character_pop_mini',
-    'buttons/button_stack_character_pop_mini.png',
-    'image'
-  );
-  this.loader.addResource(
-    'button_stack_pop_mini',
+    'mini_stack_pop',
     'buttons/button_stack_pop_mini.png',
     'image'
   );
 
-  //remover - teste
-  this.loader.addResource('block_4', 'blocks/block_4.png', 'image');
-  //this.loader.addResource('block_blank2', 'blocks/block_blank2.png', 'image');
+  //MAPS
 
-  //sprits container
-  this.loader.addResource(
-    'container_4_fields',
-    'containers/container_4_fields.png',
-    'image'
-  );
-
-  this.loader.addResource(
-    'container_5_fields',
-    'containers/container_5_fields.png',
-    'image'
-  );
-
-  //Mapas
+  //first level
   this.loader.addResource('map_level_1', 'maps/map_level_1.xml', 'xml');
 };
 //Quando o loading dos recursos acaba, esse métodos é executado
 se.gameReady = function () {
   //criando um level
   lv1 = new Level1();
-  
+
   //adicional o level ao jogo
-  this.mlevel.addScene(lv1.getScene() );
+  this.mlevel.addScene(lv1.getScene());
 };
 
 function createCommandsButton(item, limitcommands) {
-	
   new Button(
     'button_' + item,
     window.posx,
     520,
     function () {
-		//console.log(limitcommands);
-		if(actions.length < limitcommands)
-			actions.push(item);
+      //console.log(limitcommands);
+      if (actions.length < limitcommands) actions.push(item);
     },
     window.size,
     window.size
@@ -231,44 +194,48 @@ function createCommandsButton(item, limitcommands) {
 }
 
 function createGUIButton() {
-	
-	new Rect(0, 500, 100, 660, 'rgba(255, 255, 255, 0.5)');
-	new Rect(0, 0, 110, 1200, '#30415d');
-	new Rect(880, 150, 580, 1180, 'rgba(255, 255, 255, 0.5)');
+  new Rect(0, 500, 100, 660, 'rgba(255, 255, 255, 0.5)');
+  new Rect(0, 0, 110, 1200, '#30415d');
+  new Rect(880, 150, 580, 1180, 'rgba(255, 255, 255, 0.5)');
 
-	new Rect(0, 470, 40, 200, '#30415d');
-	new Text('Comandos', 20, 495, '#fff');
+  new Rect(0, 470, 40, 200, '#30415d');
+  new Text('Comandos', 20, 495, '#fff');
 
-	new Rect(900, 120, 40, 200, '#30415d');
-	new Text('Principal', 920, 145, '#fff');
+  new Rect(900, 120, 40, 200, '#30415d');
+  new Text('Principal', 920, 145, '#fff');
 
-  
-	new Button('button_play',  900,  20,
-		function () {
-		  lv1.getCodebo().setCommands(window.actions, lv1.getMap().getMap() );
-		  lv1.getCodebo().reset();
-		  
-		  lv1.getMap().reset( lv1.getCodebo());
-		  lv1.getCodebo().start();
-		  
-		   
-		},
-	70, 70 );
+  new Button(
+    'button_play',
+    900,
+    20,
+    function () {
+      lv1.getCodebo().setCommands(window.actions, lv1.getMap().getMap());
+      lv1.getCodebo().reset();
 
-	//FIXOS DE TODOS OS LEVEL
-	new Button('button_stop', 980, 20, function () {}, 60, 60);
-	new Button('button_reload', 1050, 20, 
-		function () {
-			
-			 lv1.getCodebo().reset();
-			 lv1.getMap().reset();
-			 actions = []
-			
-		}, 
-	60, 60);
-	
-	new Button('button_help', 20, 20, function () {}, 70, 70);
-	new Button('button_menu', 110, 20, function () {}, 70, 70);
+      lv1.getMap().reset(lv1.getCodebo());
+      lv1.getCodebo().start();
+    },
+    70,
+    70
+  );
+
+  //FIXOS DE TODOS OS LEVEL
+  new Button('button_stop', 980, 20, function () {}, 60, 60);
+  new Button(
+    'button_reload',
+    1050,
+    20,
+    function () {
+      lv1.getCodebo().reset();
+      lv1.getMap().reset();
+      actions = [];
+    },
+    60,
+    60
+  );
+
+  new Button('button_help', 20, 20, function () {}, 70, 70);
+  new Button('button_menu', 110, 20, function () {}, 70, 70);
 }
 
 /*Exibe os comandos abaixo do play*/
@@ -284,16 +251,16 @@ function printCommands(limitcommand) {
   var marginx = 900;
   var marginy = 180;
   var j = 0;
-  var size = 40;	
-  
+  var size = 40;
+
   //percorrendo os comandos
   for (var i = 0; i < actions.length; i++) {
     j++;
-	
+
     //grade de 4 comandos
     if (i % 6 == 0) j = 0;
-    posy = parseInt(i / 6) * (size+5);
-    posx = j * (size+5);
+    posy = parseInt(i / 6) * (size + 5);
+    posx = j * (size + 5);
 
     bt = new MiniButton(
       'mini_' + actions[i],
@@ -304,21 +271,21 @@ function printCommands(limitcommand) {
       },
       size,
       size
-    ); 
+    );
     bt.setId(i);
     bt.setTag('btcommands');
   }
-  
+
   for (var i = actions.length; i < limitcommand; i++) {
     j++;
 
     //grade de 4 comandos
     if (i % 6 == 0) j = 0;
-    posy = parseInt(i / 6) * (size+5);
-    posx = j * (size+5);
+    posy = parseInt(i / 6) * (size + 5);
+    posx = j * (size + 5);
 
     bt = new MiniButton(
-      'mini_blank' ,
+      'mini_blank',
       marginx + posx,
       marginy + posy,
       function () {
@@ -326,12 +293,10 @@ function printCommands(limitcommand) {
       },
       size,
       size
-    ); 
+    );
     bt.setId(i);
     bt.setTag('btcommands');
   }
-  
-  
 }
 
 function createArrayMap(map) {
