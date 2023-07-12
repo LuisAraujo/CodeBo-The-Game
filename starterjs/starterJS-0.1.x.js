e924017716d5f62632d7c6c917b56ecd40e3aafc
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2023-07-12 00:13:22 -0300

    fix bug by refatoring glboardjs

--- starterjs/starterJS-0.1.x.js
+++ starterjs/starterJS-0.1.x.js
@@ -55,9 +55,9 @@ Animation.prototype.verifySprites = function (sprites) {
         for(var i=0; i< sprites.length; i++){
 
             var sprite_temp =  se.loader.getAssets(sprites[i]) ;
-
+          
             if((!sprite_temp instanceof Image) || (sprite_temp == null)){
-                throw new Error("Ocorreu um erro ao carregar a imagem" + sprite_temp + ". Verifique o nome adicionado aos resources");
+                throw new Error("Ocorreu um erro ao carregar a imagem " + sprites[i] + ". Verifique o nome adicionado aos resources");
             }else{
                 this.sprites.push(sprite_temp);
             }
@@ -2314,13 +2314,14 @@ ManagerLoader.prototype.loading = function( ){
 
     if(this.assetsList.length == 0 ) {
         this.print(0.99);
+        console.log("waiting load")
         setTimeout(this.callback, 2000);
         return;
     }
 
     //console.log(this.assetsList);
     for(var i = 0; i < this.assetsList.length; i++){
-
+        
         //se for do tipo imagem
 		if(this.assetsList[i][2] == "image"){
 
@@ -2495,11 +2496,12 @@ ManagerLoader.prototype.loading = function( ){
  * @returns {Object}
  */
 ManagerLoader.prototype.getAssets = function(name){
-
+  
     var returning = null;
     for(var i=0; i< this.assetsList.length; i++){
-        if(this.assetsList[i][0] == name)
+        if(this.assetsList[i][0] == name){
             returning = this.assetsListLoaded[i];
+        }
     }
 
     return returning;
@@ -4615,20 +4617,20 @@ Mobile.prototype.closeFullScreen = function(){
  * @param {int} w - Largura da tela
  * @param {int} h - Altura da tela
  */
-StarterEngine = function (w,h) {
+StarterEngine = function (w,h,s) {
     this.width = w;
     this.height = h;
     this.debugmode = false;
     this.starterlogo = null;
 	//exceute in smartphone as mobile?
 	this.asmobile = false;
+    this.startonload = s;
 
 	/**@global */
 	canvas = null;
 	
-    window.onload = function(){
-        
-	
+    this.mainfunction = function(){
+       
 		canvas = document.createElement("canvas");
 		canvas.id = "gamecanvas";
 		canvas.style="background: #909090";
@@ -4657,12 +4659,15 @@ StarterEngine = function (w,h) {
         this.teclado = new ManagerInputs();
         this.storage = new ManagerStorage();
 
+        console.log(this);
+
         //iniciando o jogo
         this.setResources();
-        this.beginLoad();
+        
     
 	}.bind(this);
 
+        
 	/**@global */
     red = {r:255,b:0,g:0};
 	/**@global */
@@ -4689,6 +4694,11 @@ StarterEngine = function (w,h) {
 	this.landscape = true;
 
 
+    if( this.startonload)
+        window.onload = this.mainfunction;
+    else
+        this.mainfunction();
+        
 };
 
 
@@ -4706,6 +4716,7 @@ StarterEngine.prototype.setAsMobile =function (asmobile) {
  * @method
  */
 StarterEngine.prototype.beginLoad =function () {
+    console.log("begin load");
     this.loader.loading();
 }
 
@@ -4771,7 +4782,491 @@ StarterEngine.prototype.gameReady = function() {};
  * Configura a lista de arquivos (imagens e sons) a serem carregados
  * @method
  */
-StarterEngine.prototype.setResources =function () {};
+StarterEngine.prototype.setResources = function () {
+
+    
+   _this = this;
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "assets/resourcelist.csv" , false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+            
+                var lineCSV = null;
+                var allText = rawFile.responseText;
+
+                //brake by line
+                lineCSV = allText.split("\n");
+
+                lineCSV.forEach(function (line) {
+                    //break by ;
+                    var elem = line.split(";");
+                    if(elem.length>1)
+                    _this.loader.addResource(elem[0], elem[1], elem[2].slice(0,-1) );
+                    
+                });
+
+                this.beginLoad();
+            }
+        }
+    }.bind(this);
+    rawFile.send(null);
+    /**/
+
+/*
+//menu
+  this.loader.addResource('button_play_menu', 'buttons/gui_bt_play_menu.png', 'image');
+  this.loader.addResource('button_play_menu2', 'buttons/gui_block_play_menu.png', 'image');
+  
+  this.loader.addResource('button_help_menu',  'buttons/gui_bt_ajuda.png', 'image');
+  this.loader.addResource('button_config_menu',  'buttons/gui_bt_config.png', 'image');
+  
+  this.loader.addResource('gui_logo',  'gui/logo.png', 'image');
+  this.loader.addResource('gui_logo_bg',  'gui/logo_bg.png', 'image');
+  
+  this.loader.addResource('gui_codebo',  'gui/gui_codebo_menu.png', 'image');
+  this.loader.addResource('gui_bg_menu',  'gui/gui_bg_menu.png', 'image');
+  this.loader.addResource('gui_block1',  'gui/gui_block_plan_1.png', 'image');
+  this.loader.addResource('gui_block3a',  'gui/gui_block_plan_3_a.png', 'image');
+  this.loader.addResource('gui_block3b',  'gui/gui_block_plan_3_b.png', 'image');
+   this.loader.addResource('gui_block4a',  'gui/gui_block_plan_4_a.png', 'image');
+  this.loader.addResource('gui_block4b',  'gui/gui_block_plan_4_b.png', 'image');
+  this.loader.addResource('gui_block5',  'gui/gui_block_plan_5.png', 'image');
+  this.loader.addResource('gui_floor',  'gui/gui_floor.png', 'image');
+  this.loader.addResource('gui_panel_menu',  'gui/gui_panel_menu.png', 'image');
+  this.loader.addResource('gui_block2a',  'gui/gui_block_plain_2.png', 'image');
+  this.loader.addResource('gui_block2b',  'gui/gui_block_plain_2_b.png', 'image');
+  this.loader.addResource('gui_block2c',  'gui/gui_block_plain_2_c.png', 'image');
+
+  
+  //CHARACTER
+
+  //CodeBo pra frente
+  this.loader.addResource('codebo_sp1', 'codebo/forward/sp1.png', 'image');
+  this.loader.addResource('codebo_sp2', 'codebo/forward/sp2.png', 'image');
+  this.loader.addResource('codebo_sp3', 'codebo/forward/sp3.png', 'image');
+  this.loader.addResource('codebo_sp4', 'codebo/forward/sp4.png', 'image');
+  this.loader.addResource('codebo_sp5', 'codebo/forward/sp5.png', 'image');
+  this.loader.addResource('codebo_sp6', 'codebo/forward/sp6.png', 'image');
+
+  //CodeBo pra trás
+  this.loader.addResource(
+    'codebo_back_sp1',
+    'codebo/backward/sp1.png',
+    'image'
+  );
+
+  //CodeBo pra esquerda
+  this.loader.addResource('codebo_left_sp1', 'codebo/left/sp1.png', 'image');
+
+  //CodeBo pra direita
+  this.loader.addResource('codebo_right_sp1', 'codebo/right/sp1.png', 'image');
+  this.loader.addResource('codebo_right_sp2', 'codebo/right/sp2.png', 'image');
+  this.loader.addResource('codebo_right_sp3', 'codebo/right/sp3.png', 'image');
+  this.loader.addResource('codebo_right_sp4', 'codebo/right/sp4.png', 'image');
+  this.loader.addResource('codebo_right_sp5', 'codebo/right/sp5.png', 'image');
+  this.loader.addResource('codebo_right_sp6', 'codebo/right/sp6.png', 'image');
+
+  //BLOCKS
+
+  //earth blocks
+  this.loader.addResource('block_0', 'blocks/block_0.png', 'image');
+  this.loader.addResource('block_1', 'blocks/block_1.png', 'image');
+  this.loader.addResource('block_2', 'blocks/block_2.png', 'image');
+  this.loader.addResource('block_3', 'blocks/block_3.png', 'image');
+
+  //metal blocks
+  this.loader.addResource('block_4', 'blocks/block_4.png', 'image');
+  this.loader.addResource('block_shadow', 'blocks/block_shadow.png', 'image');
+  this.loader.addResource('block_shadow2', 'blocks/block_shadow5.png', 'image');
+  
+  this.loader.addResource('block_bridge', 'blocks/bridge_0.png', 'image');
+  this.loader.addResource('block_bridge_shadow', 'blocks/bridge_shadow.png', 'image');
+  
+  //this.loader.addResource('block_blank2', 'blocks/block_blank2.png', 'image');
+
+  //water blocks
+  this.loader.addResource('water_0', 'water/water_0.png', 'image');
+  this.loader.addResource('water_1', 'water/water_1.png', 'image');
+  this.loader.addResource('water_2', 'water/water_2.png', 'image');
+
+  //BUTTONS
+
+  //control buttons
+  this.loader.addResource('button_play', 'buttons/button_play.png', 'image');
+  this.loader.addResource('button_stop', 'buttons/button_stop.png', 'image');
+  this.loader.addResource('button_help', 'buttons/button_help.png', 'image');
+  this.loader.addResource(
+    'button_reload',
+    'buttons/button_reload.png',
+    'image'
+  );
+  this.loader.addResource('button_menu', 'buttons/button_menu.png', 'image');
+
+  //movement buttons
+  this.loader.addResource(
+    'button_forward',
+    'buttons/button_forward.png',
+    'image'
+  );
+  this.loader.addResource('button_left', 'buttons/button_left.png', 'image');
+  this.loader.addResource('button_right', 'buttons/button_right.png', 'image');
+
+  //list buttons
+  this.loader.addResource(
+    'button_list_new',
+    'buttons/button_list_new.png',
+    'image'
+  );
+  this.loader.addResource(
+    'button_list_block_add',
+    'buttons/button_list_block_add.png',
+    'image'
+  );
+  this.loader.addResource(
+    'button_list_block_remove',
+    'buttons/button_list_block_remove.png',
+    'image'
+  );
+
+  //stack buttons
+  this.loader.addResource(
+    'button_stack_new',
+    'buttons/button_stack_new.png',
+    'image'
+  );
+  this.loader.addResource(
+    'button_stack_block_push',
+    'buttons/button_stack_block_push.png',
+    'image'
+  );
+  this.loader.addResource(
+    'button_stack_character_push',
+    'buttons/button_stack_character_push.png',
+    'image'
+  );
+  this.loader.addResource(
+    'button_stack_pop',
+    'buttons/button_stack_pop.png',
+    'image'
+  );
+
+  //MINI BUTTONS
+
+  //movement mini buttons
+  this.loader.addResource(
+    'mini_hightlight',
+    'buttons/buttons_hightlight.png',
+    'image'
+  );
+  
+  
+  this.loader.addResource(
+    'mini_forward',
+    'buttons/button_forward_mini.png',
+    'image'
+  );
+  
+  
+  this.loader.addResource('mini_left', 'buttons/button_left_mini.png', 'image');
+  this.loader.addResource(
+    'mini_right',
+    'buttons/button_right_mini.png',
+    'image'
+  );
+
+  this.loader.addResource('mini_blank', 'buttons/button_mini.png', 'image');
+
+  //list mini buttons
+  this.loader.addResource(
+    'button_list_new_mini',
+    'buttons/button_list_new_mini.png',
+    'image'
+  );
+  this.loader.addResource(
+    'button_list_block_add_mini',
+    'buttons/button_list_block_add_mini.png',
+    'image'
+  );
+  this.loader.addResource(
+    'button_list_block_remove_mini',
+    'buttons/button_list_block_remove_mini.png',
+    'image'
+  );
+
+  //stack mini buttons
+  this.loader.addResource(
+    'mini_stack_new',
+    'buttons/button_stack_new_mini.png',
+    'image'
+  );
+  this.loader.addResource(
+    'mini_stack_block_push',
+    'buttons/button_stack_block_push_mini.png',
+    'image'
+  );
+  this.loader.addResource(
+    'mini_stack_character_push',
+    'buttons/button_stack_character_push_mini.png',
+    'image'
+  );
+  
+  this.loader.addResource(
+    'mini_stack_pop',
+    'buttons/button_stack_pop_mini.png',
+    'image'
+  );
+
+  //MAPS
+
+  //first level
+  this.loader.addResource('map_level_1', 'maps/map_level_1.xml', 'xml');
+  this.loader.addResource('map_level_2', 'maps/map_level_2.xml', 'xml');
+  this.loader.addResource('map_level_3', 'maps/map_level_3.xml', 'xml');
+  this.loader.addResource('map_level_4', 'maps/map_level_4.xml', 'xml');
+  this.loader.addResource('map_level_5', 'maps/map_level_5.xml', 'xml');
+  this.loader.addResource('map_level_6', 'maps/map_level_6.xml', 'xml');
+  this.loader.addResource('map_level_7', 'maps/map_level_7.xml', 'xml');
+  this.loader.addResource('map_level_8', 'maps/map_level_8.xml', 'xml');
+  this.loader.addResource('map_level_9', 'maps/map_level_9.xml', 'xml');
+  this.loader.addResource('map_level_10', 'maps/map_level_10.xml', 'xml');
+
+  //FLAGS
+
+  this.loader.addResource(
+    'red_flag',
+    'flags/red-flag-01.png',
+    'image'
+  );
+  
+   this.loader.addResource(
+    'red_flag2',
+    'flags/red-flag-02.png',
+    'image'
+  );
+  
+  
+  
+   this.loader.addResource(
+    'red_flag3',
+    'flags/red-flag-03.png',
+    'image'
+  );
+  
+  
+  this.loader.addResource(
+    'balon_msg',
+    'gui/balon_msg.png',
+    'image'
+  );
+  
+  
+  
+  this.loader.addResource(
+    'bt_close_msg',
+    'gui/bt_close_msg.png',
+    'image'
+  );
+  
+  
+  
+  this.loader.addResource(
+    'bt_next_msg',
+    'gui/bt_next_msg.png',
+    'image'
+  );
+  
+    this.loader.addResource(
+    'bt_prior_msg',
+    'gui/bt_prior_msg.png',
+    'image'
+  );
+  
+  //PLACEHOLDERS DAQUI --->>>
+  
+  //ITEM
+   this.loader.addResource(
+    'item_bridge',
+    'items/item_1.png',
+    'image'
+  );
+  
+  //CONATINER
+   this.loader.addResource(
+    'container_item',
+    'gui/container_item.png',
+    'image'
+  );
+  
+  //GUI BLOCK
+   this.loader.addResource(
+    'gui_block',
+    'gui/gui_block.png',
+    'image'
+  );
+  
+  
+    //COMMANDO SET ITEM
+   this.loader.addResource(
+    'button_set_item',
+    'buttons/button_set_item.png',
+    'image'
+  );
+  
+  
+   this.loader.addResource(
+    'mini_set_item',
+    'buttons/button_set_item_mini.png',
+    'image'
+  );
+  
+  
+  //MODAL END LEVEL
+    this.loader.addResource(
+    'bg_modal_finish',
+    'gui/bg_modal_finish.png',
+    'image'
+  );
+  //BUTTON NEXT LEVEL
+    this.loader.addResource(
+    'bt_next_level',
+    'buttons/bt_next_level.png',
+    'image'
+  );
+  //BUTTON MENU LEVELS
+    this.loader.addResource(
+    'bt_menu_level',
+    'buttons/bt_menu_level.png',
+    'image'
+  );
+  
+  //STARTS
+  
+    this.loader.addResource(
+    'gui_star_1',
+    'gui/gui_star_1.png',
+    'image'
+  );
+  
+    this.loader.addResource(
+    'gui_star_2',
+    'gui/gui_star_2.png',
+    'image'
+  );
+  
+   this.loader.addResource(
+    'gui_star_3',
+    'gui/gui_star_3.png',
+    'image'
+  );
+  
+  
+     this.loader.addResource(
+    'gui_congrats',
+    'gui/gui_congrats.png',
+    'image'
+  );
+  
+  this.loader.addResource(
+    'bg_universe',
+    'gui/universe_bg.png',
+    'image'
+  );
+  
+  
+  this.loader.addResource(
+    'world_level_1',
+    'worlds/world_level_1.png ',
+    'image'
+  ); 
+  
+  this.loader.addResource(
+    'world_level_locked',
+    'worlds/world_level_locked.png ',
+    'image'
+  );
+  
+  this.loader.addResource(
+    'mini_star',
+    'gui/mini_star.png ',
+    'image'
+  );
+  
+   this.loader.addResource(
+    'logo_unifacs',
+    'logo/unifacs.png ',
+    'image'
+  );
+  
+  
+   this.loader.addResource(
+    'logo_fapesb',
+    'logo/fapesb.png ',
+    'image'
+  );
+  
+ this.loader.addResource(
+    'logo_anim1',
+    'animation/logo_anim1.png ',
+    'image'
+  );
+  
+  this.loader.addResource(
+    'logo_anim2',
+    'animation/logo_anim2.png ',
+    'image'
+  );
+  
+  
+  this.loader.addResource(
+    'logo_anim3',
+    'animation/logo_anim3.png ',
+    'image'
+  );
+  
+  
+  this.loader.addResource(
+    'logo_anim4',
+    'animation/logo_anim4.png ',
+    'image'
+  );
+  
+  
+  this.loader.addResource(
+    'logo_anim5',
+    'animation/logo_anim5.png ',
+    'image'
+  );
+  
+  
+  this.loader.addResource(
+    'logo_anim6',
+    'animation/logo_anim6.png ',
+    'image'
+  );
+  
+  this.loader.addResource(
+    'logo_anim7',
+    'animation/logo_anim7.png ',
+    'image'
+  );
+  
+   this.loader.addResource(
+    'tutorial_criapilha01',
+    'gui/tutorial_criapilha01.png ',
+    'image'
+  );
+  
+  this.loader.addResource(
+    'tutorial_criapilha02',
+    'gui/tutorial_criapilha02.png ',
+    'image'
+  );
+  */
+};
 
 
 /**
