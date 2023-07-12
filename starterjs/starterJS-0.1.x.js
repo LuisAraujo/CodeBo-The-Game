bb25ea15a5d03143aa12b5dd37b7979529c21d26
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2023-07-12 00:54:49 -0300

    teste loader assets

--- starterjs/starterJS-0.1.x.js
+++ starterjs/starterJS-0.1.x.js
@@ -2318,10 +2318,10 @@ ManagerLoader.prototype.loading = function( ){
         setTimeout(this.callback, 2000);
         return;
     }
-
+    console.log(this.assetsList.length);
     //console.log(this.assetsList);
     for(var i = 0; i < this.assetsList.length; i++){
-        
+        console.log(this.assetsList[i])
         //se for do tipo imagem
 		if(this.assetsList[i][2] == "image"){
             console.log(this.assetsList[i])
