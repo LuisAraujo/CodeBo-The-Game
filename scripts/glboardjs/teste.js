cbe43ef97d91ab608fe57026d5fec5f6cdbfeeec
hiago <moreira.hiago08@gmail.com>
2023-07-22 16:31:33 -0300

    setting test environment

--- scripts/glboardjs/teste.js
+++ scripts/glboardjs/teste.js
@@ -5,7 +5,9 @@ import { GENDER } from "./utils/StructsCommon.js";
 const urlParams = new URLSearchParams(window.location.search);
 const playerName = urlParams.get("nome");
 
-window.gl = new GLBoard("38-g3GfglwUPqMzh3PQLbA", playerName ?? "guest" + Math.random().toString().split(".")[1]);
+// Credenciais do GLBoard - Temporariamente comentado para teste
+// window.gl = new GLBoard("38-g3GfglwUPqMzh3PQLbA", playerName ?? "guest" + Math.random().toString().split(".")[1]);
+window.gl = new GLBoard("_25QFu6_u34tDLpx2Sv-JA", playerName);
 await gl.LOAD_USER_DATA( function(){
    
     var src = "scripts/mygame.js";
