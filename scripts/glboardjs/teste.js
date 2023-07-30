522b2983ced62cff6955f4f160e94a9c44f25505
hiago <moreira.hiago08@gmail.com>
2023-07-30 15:14:56 -0300

    Changing gl credentials

--- scripts/glboardjs/teste.js
+++ scripts/glboardjs/teste.js
@@ -7,7 +7,7 @@ const playerName = urlParams.get("name");
 
 // Credenciais do GLBoard - Temporariamente comentado para teste
 // window.gl = new GLBoard("_25QFu6_u34tDLpx2Sv-JA", playerName);
-window.gl = new GLBoard("38-g3GfglwUPqMzh3PQLbA", playerName ?? "guest" + Math.random().toString().split(".")[1]);
+window.gl = new GLBoard("45X1SnmfszX-DM0Ezj-XtQ", playerName ?? "guest" + Math.random().toString().split(".")[1]);
 await gl.LOAD_USER_DATA( function(){
    
     var src = "scripts/mygame.js";
@@ -24,7 +24,7 @@ if (gl.data.player_data.id === undefined) {
     const dateBirthday = birthday ?? "01/01/2023";
     
     gl.setPlayerData(
-        playerName,
+        gl.USER_ID,
         dateBirthday + " 00:00:00",
         gender ?? "OUTROS"
     );
